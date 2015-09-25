906f7aaff4b72be59893c7d19561680cf9eec15d
Brittany Johnson <BrittJay@Brittanys-MacBook-Pro.local>
2015-09-25 11:34:19 -0400

    file to test proper addition of file to repository

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+	
+	private static getValue(){
+
+		try {
+
+			//do something
+		}
+		catch (NullPointerException){
+			System.out.println("NullPointerException found!")
+		}
+	}
+}
