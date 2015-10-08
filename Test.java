ca6943e2c0d57ca0f7f93b50c43a66c67bb75584
Brittany Johnson <BrittJay@Brittanys-MacBook-Pro.local>
2015-10-08 12:10:50 -0400

    removed catch block -- other one not picked up because of spacing issue :(

--- Test.java
+++ Test.java
@@ -2,14 +2,9 @@ public class Test {
 	
 	private static getValue(){
 
-		try {
 
 			String s = "Hello World";
 			Object o = new Object();
-			
-		}
-		catch (NullPointerException e){
-			System.out.println("NullPointerException found!")
-		}
+
 	}
 }
