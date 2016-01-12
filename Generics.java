541010765841f100ae169617bef0b74658fcf45e
Brittany Johnson <Brittany Johnson>
2016-01-12 13:12:51 -0500

    added more generics

--- Generics.java
+++ Generics.java
@@ -15,7 +15,32 @@ public class Box<T> {
 	  List<T> list = new ArrayList<T>();
   }
   
- 
+  // determines the largest of three Comparable objects
+   public static <T extends Comparable<T>> T maximum(T x, T y, T z)
+   {                      
+      T max = x; // assume x is initially the largest       
+      if ( y.compareTo( max ) > 0 ){
+         max = y; // y is the largest so far
+      }
+      if ( z.compareTo( max ) > 0 ){
+         max = z; // z is the largest now                 
+      }
+      return max; // returns the largest object   
+   }
+   
+   public static double sum(List<? extends Number> list){
+       double sum = 0;
+       for(Number n : list){
+           sum += n.doubleValue();
+       }
+       return sum;
+   }
+   
+   public static void printData(List<?> list){
+       for(Object obj : list){
+           System.out.print(obj + "::");
+       }
+   }
 
   public static void main(String[] args) {
      
